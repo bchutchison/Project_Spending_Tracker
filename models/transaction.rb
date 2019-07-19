65e2624d53e90db94500be9f437eb4a6c01510e2
bchutchison <benhutchison85@gmail.com>
2019-07-19 14:14:26 +0100

    tansaction.rb setup complete

--- models/transaction.rb
+++ models/transaction.rb
@@ -1,1 +1,32 @@
 require_relative('../db/sql_runner')
+
+class Transaction
+
+  attr_reader :id
+  attr_accessor :tag_id, :merchant_id, :value
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @tag_id = options['tag_id'].to_i
+    @merchant_id = options['merchant_id'].to_i
+    @value = options['value'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO transactions
+    (tag_id, merchant_id, value)
+    VALUES
+    ($1,$2,$3)
+    RETURNING id"
+    values = [@tag_id, @merchant_id, @value, @id]
+    transaction = SqlRunner.new( sql, values )
+    @id = transaction.first()['id'].to_i
+  end
+
+
+  def self.delete_all()
+    sql = "DELETE FROM transactions"
+    SqlRunner.run( sql )
+  end
+
+end
